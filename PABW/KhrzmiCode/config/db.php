f6513735b0ea9c711fcc56276a6fae240736d050
Haisyam <muhamadhaisyamkhairizmi@gmail.com>
2025-10-03 15:58:50 +0700

    add: config

---
+++ PABW/KhrzmiCode/config/db.php
@@ -0,0 +1,11 @@
+<?php
+$hostname = "localhost";
+$username = "root";
+$password = "";
+$dbname = "icam";
+
+$conn = new mysqli($hostname, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
